be4b4e1d8e5d02ace557cfd424a40d8afa8a6966
Igor <igor_vf@outlook.com>
2021-06-26 17:06:25 -0300

    iniciando nlw

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import {Button} from './components/Button'
+import { Home } from './pages/Home';
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
